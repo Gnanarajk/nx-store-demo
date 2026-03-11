706d9452324940913b49349ce26b025cec01bb74
Gnanaraj <gnanaraj.k@gmail.com>
2026-03-11 09:32:21 +0530

    environments added

--- apps/host/webpack.prod.config.ts
+++ apps/host/webpack.prod.config.ts
@@ -3,7 +3,7 @@ import { withModuleFederation } from '@nx/module-federation/angular';
 export default withModuleFederation({
   name: 'host',
   remotes: [
-    ['productsRemote', 'https://nxstore-products.azurewebsites.net/remoteEntry.js'],
-    ['productDetailRemote', 'https://nxstore-productdetail.azurewebsites.net/remoteEntry.js'],
+    ['productsRemote', 'https://nxstore-products.azurewebsites.net/remoteEntry.mjs'],
+    ['productDetailRemote', 'https://nxstore-productdetail.azurewebsites.net/remoteEntry.mjs'],
   ],
 }, { dts: false });
